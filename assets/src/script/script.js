5e760777d220849c15b6f505f14f440e14802f48
Syllas Braga <syllasbraga2@gmail.com>
2022-07-29 19:15:33 -0300

    Consertando os bugs que estavam presentes no slider

--- assets/src/script/script.js
+++ assets/src/script/script.js
@@ -4,40 +4,29 @@ const BTN_NEXT = document.getElementById("next");
 const BTN_BACK = document.getElementById("back");
 
 let atualImg = 0;
-let proxImg = 1;
-
 
 function nextImg() {
-    if (proxImg>IMG.length-1) {
+    if (atualImg>=IMG.length-1) {
         IMG[atualImg].classList.remove("imgVisivel");
         atualImg = 0;
         IMG[atualImg].classList.add("imgVisivel");
-        proxImg=1;
-        console.log("ProxImg: "+proxImg+". AtualImg: "+atualImg+" if nextImg")
     }else{
         IMG[atualImg].classList.remove("imgVisivel");
-        IMG[proxImg].classList.add("imgVisivel");
         atualImg++;
-        proxImg++;
-        console.log("ProxImg: "+proxImg+". AtualImg: "+atualImg+" nextImg");
+        IMG[atualImg].classList.add("imgVisivel");
     }
-
 }
+
 function backImg() {
     if (atualImg<=0) {
         IMG[atualImg].classList.remove("imgVisivel");
-        atualImg = IMG.length-1;
+        atualImg = 5;
         IMG[atualImg].classList.add("imgVisivel");
-        proxImg = IMG.length;
-        console.log("ProxImg: "+proxImg+". AtualImg: "+atualImg+" if backImg");
     }else{
         IMG[atualImg].classList.remove("imgVisivel");
         atualImg--;
-        IMG[proxImg].classList.add("imgVisivel");
-        proxImg--;
-        console.log("ProxImg: "+proxImg+". AtualImg: "+atualImg+" backImg");
+        IMG[atualImg].classList.add("imgVisivel");
     }
-
 }
 
 BTN_NEXT.addEventListener("click", nextImg);
